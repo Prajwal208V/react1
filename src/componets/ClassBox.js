7b9ebf0b9f1e9e68c49af3178170c9aad124c35c
Prajwal208V <prajwalv208@gmail.com>
2022-02-22 13:45:35 +0530

    second-commit

--- src/componets/ClassBox.js
+++ src/componets/ClassBox.js
@@ -1,8 +1,6 @@
 import React,{Component} from 'react';
-const para2={
-    color:"blue",
-    fontSize:"1.1rem"
-}
+import InsideClassBox from './InsideClassBox';
+
 class ClassBox extends Component { 
     state={
         displayProp:"box-none",
@@ -15,16 +13,21 @@ class ClassBox extends Component {
     }
 
     render() {  
-        return(
-        <div class="class-compoent">
-            <button className="button" onClick={()=>{this.clickHandler()}}>To see styling in class componet</button>
-            <div className={this.state.displayProp} id="cls_box">
-                <h1 className="box_header">This is created using class componet</h1>
-                <p className="para1"> This is done using External CSS</p>
-                <p  style={para2}> This is done using Inline CSS</p>
-            </div>
-        </div>
-        )
+        if(this.state.displayProp==='box'){
+            return(
+                <div class="class-compoent">
+                    <button className="button" onClick={()=>{this.clickHandler()}}>To see styling in class componet</button>
+                    <InsideClassBox />
+                </div>
+            )
+        }
+        else{
+            return(
+                <div class="class-compoent">
+                    <button className="button" onClick={()=>{this.clickHandler()}}>To see styling in class componet</button>
+                </div>
+            )
+        }
     } 
 }
 export default ClassBox;
--- src/componets/FunctionBox.js
+++ src/componets/FunctionBox.js
@@ -2,25 +2,41 @@ import React, { useState } from 'react';
 const para2 = {
   color: "blue",
 }
-const FunctionBox = () => {
-  const [properties, setProperty] = useState("box-none");
-
-  const clickHandler = () => {
-    let temp = (properties === "box") ? "box-none" : "box";
-    setProperty(temp);
-  }
-  return (
-    <div class="funct-compoent">
-      <button className="button" onClick={() => { clickHandler() }}>
-        To see styling in functional componet
-      </button>
-      <div className={properties}>
+const InsideBox=()=>{
+    return(
+      <div className="box">
         <h1 className="box_header" >This is created using functional componet</h1>
         <p className="para1"> This is done using External CSS</p>
         <p style={para2}> This is done using Inline CSS</p>
       </div>
-    </div>
-  )
+    )
+}
+const FunctionBox = () => {
+  const [properties, setProperty] = useState("box-none");
+  const clickHandler = () => {
+    let temp = (properties === "box") ? "box-none" : "box";
+    setProperty(temp);
+  }
+  if(properties==="box"){
+    return (
+      <div class="funct-compoent">
+        <button className="button" onClick={() => { clickHandler() }}>
+          To see styling in functional componet
+        </button>
+          <InsideBox />
+      </div>
+    )
+  }
+  else{
+    return(
+      <div class="funct-compoent">
+          <button className="button" onClick={() => { clickHandler() }}>
+            To see styling in functional componet
+          </button>
+      </div>
+    )
+  }
+  
 }
 
 export default FunctionBox;
